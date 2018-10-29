02e8d35c069176ec8416d09903ace681937746f3
tilman ersig <ersigtf@g.cofc.edu>
2018-10-28 18:40:42 -0800

    finished version

--- action.js
+++ action.js
@@ -6,10 +6,49 @@ function checkEmail(email) {
 
 function checkAge() {
     var regEx = new RegExp('^(([0-9][1-9])|([1-9][0-9])|[1-9])$');
-    
-    if() {
-       
-    } else {
-       
+    var age = document.getElementById("age").value;
+    var i;
+    var incorrectValue = false;
+    for (i=0; i < age.length; i++){
+        if (age.charAt(i)== '.'){
+            incorrectValue = true;
+        }
     }
+    if((i>3) || incorrectValue) {
+       document.getElementById("age").style.backgroundColor = "red";
+    } else {
+        document.getElementById("age").style.backgroundColor = "rgba(255,255,255,.5)";
+    }
+}
+
+function checkName() {
+    var nameString = document.getElementById("name").value;
+    for (var i = 0; i < nameString.length; i++){
+        if (nameString.charAt(i) == '<'){
+            document.getElementById("name").style.backgroundColor = "red";
+        }
+        else {
+            document.getElementById("name").style.backgroundColor = "rgba(255,255,255,.5)";
+        }
+    }
+}
+
+function validate() {
+    var email = document.getElementById("email").value;
+    var goodEmail = checkEmail(email);
+
+    if (goodEmail){
+        myAjaxFunction();
+    }
+    else {
+        window.alert("The information could not be processed. Please review and correct any mistakes.")
+    }
+}
+
+function myAjaxFunction() {
+    window.alert("The AJAX has been created.")
+    document.getElementById("name").reset();
+    document.getElementById("age").reset();
+    document.getElementById("email").reset();
+
 }
